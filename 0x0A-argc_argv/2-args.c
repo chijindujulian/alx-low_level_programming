c3f54b22aa92524b6fd9b0e54d6054d0f7629e4e
chijindu julian <chijindujulian@gmail.com>
2024-01-15 10:52:58 -0800

    add 2-args.c function

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * @argc: calculates total number of command line args
+ * @argv: array of pointers to strings
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+        	printf("%s\n", argv[i]);
+	}
+
+        return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,4 @@
 0x0A - ARGC, ARGV
 0 - whatsmyname.c
 1 - args.c
+2 - args.c 
