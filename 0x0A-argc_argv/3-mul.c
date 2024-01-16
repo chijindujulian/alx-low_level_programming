b988778d069a4c36408af076cb0d9d8884cfc6c9
chijindu julian <chijindujulian@gmail.com>
2024-01-15 21:03:11 -0800

    fix 3-mul.c function

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -53,24 +53,21 @@ int _atoi(char *s)
  *
  * Return: 0 (Success), 1 (Error)
  */
-
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		return (1);
 	}
 
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
-
 	result = num1 * num2;
 
 	printf("%d\n", result);
 
 	return (0);
 }
-
