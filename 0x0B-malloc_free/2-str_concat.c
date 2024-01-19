244807846fdc8da729a4bcd9d53c13e106418370
chijindu julian <chijindujulian@gmail.com>
2024-01-19 07:51:18 -0800

    fix-3 2-str_concat.c function

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,44 +10,40 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i, j, len1, len2;
-	char *str;
-	/*get the length of string in s1*/
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-	{}
-	/*get length of s2*/
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-	{}
-	/*dynamic allocation of mem to str*/
-	str = (char *) malloc((len1 + len2 + 1) * sizeof(char));
-	/*check if str is null*/
-	if (str == NULL)
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
 		return (NULL);
-	/*check if s1 is NULL and return empty string*/
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	/*if s1 is not NUll, copy s1 index to str index*/
+
+	i = 0;
+	j = 0;
+
 	if (s1)
 	{
-		for (i = 0; i < len1; i++)
+		while (i < len1)
 		{
-			str[i] = s1[i];
-		}
-	}
-	if (s2)
-	{
-		for (j = 0; j < (len1 + len2); j++)
-		{
-			str[i] = s2[j];
+			s3[i] = s1[i];
 			i++;
 		}
 	}
-	str[i] = '\0';
-	return (str);
+
+	if (s2)
+	{
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	s3[i] = '\0';
+
+	return (s3);
 }
