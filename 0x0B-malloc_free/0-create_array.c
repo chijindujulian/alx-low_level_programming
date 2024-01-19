376edede8fcaf53e4b016f6a4ffe6e324f2dc646
chijindu julian <chijindujulian@gmail.com>
2024-01-18 23:03:12 -0800

    fix 0-create_array.c function

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -25,10 +25,10 @@ char *create_array(unsigned int size, char c)
 
 	for (i = 0; i < size; i++)
 	{
-		arr[i] = c;
+		arr[i + 1] = c;
 	}
-	arr[i] = '\0';
-	free(arr);
+	arr[i + 1] = '\0';
+
 	return (arr);
 }
 
