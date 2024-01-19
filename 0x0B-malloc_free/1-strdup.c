e040ad4bfdfc077d12778d48eb146fc86d99807a
chijindu julian <chijindujulian@gmail.com>
2024-01-18 23:50:17 -0800

    add 1-strdup.c function

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup - duplicate the string given as argument
+ * @str: string to duplicate
+ *
+ * Return: pointer to the duplicated string (Success), NULL (Error)
+ */
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *dup;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{}
+
+	dup = (char *)malloc(i * sizeof(char));
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+	{
+		dup[j] = str[j];
+	}
+	dup[j] = '\0';
+
+	return (dup);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,2 +1,3 @@
 0x0B - MALLOC_FREE
 0 - create_array
+1 - strdup.c
