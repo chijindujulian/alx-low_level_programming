837be2418423456fad61ee34ab8a0566023bc47e
chijindu julian <chijindujulian@gmail.com>
2024-01-21 23:56:23 -0800

    add 3-alloc_grid.c function

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,9 +5,7 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
-<<<<<<< HEAD
+
 char *argstostr(int ac, char **av);
-=======
->>>>>>> 06f55d6b83cbaef1e5f200b7cd12adaaf0d8d7a0
 
 #endif
