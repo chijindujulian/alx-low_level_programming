2ca81674d7d84deaadcb3b8e3d6872ead1c0afc9
chijindu julian <chijindujulian@gmail.com>
2024-01-23 13:55:53 -0800

    fix dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - a dog's basic info
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: Full description
+ */
 
-struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
 #endif
