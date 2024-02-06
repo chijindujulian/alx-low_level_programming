95ba96fb2aaea416124c7ab3d949156fa8e79efb
chijindu julian <chijindujulian@gmail.com>
2024-02-06 06:19:45 -0800

    add 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - print a list of integers in singly linked list
+ * @h: pointer to the singly linked list
+ *
+ * Return: number of nodes in the singly linked list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
